c7c63d377e875761d9083fa7dcef0ab82406c7b1
7erp1c <ul_tray@bk.ru>
2024-03-14 19:11:14 +0300

    Basic authorisation

--- src/middleware/authGuardMiddleware.ts
+++ src/middleware/authGuardMiddleware.ts
@@ -3,17 +3,15 @@ import {header} from "express-validator";
 
 export const authGuardMiddleware = (req: Request,res: Response,next: NextFunction ) =>{
     const authHeader: any = req.headers.authorization;
-    console.log("authHeader: " + authHeader)
+    const oneAuthHeader = authHeader.split(' ').splice(0,1).toString()
+    console.log('_____________________' + oneAuthHeader)
     if (!authHeader) {
        res.status(401).json({ error: 'Unauthorized' });
     }
     const credentials = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    console.log("credentials: " + credentials)
     const username = credentials[0];
-    console.log("username: " + username)
     const password = credentials[1];
-    console.log("password: " + password)
-    if (username !== 'admin' || password !== 'qwerty') {
+    if ( oneAuthHeader !=='Basic'|| username !== 'admin' || password !== 'qwerty') {
          res.status(401).json({ error: 'Unauthorized' });
     }
     next();
