a74061dcbb61a0535cfbb9f3f68bc6ab0f48135b
7erp1c <ul_tray@bk.ru>
2024-03-14 19:22:56 +0300

    repair Validation 1

--- src/middleware/errorsValidation.ts
+++ src/middleware/errorsValidation.ts
@@ -12,7 +12,7 @@ export const errorsValidation = (req:Request, res:Response, next:NextFunction) =
             errors.array({onlyFirstError:true}).forEach((error) => {
                 errorMessages.push({ message: 'Bad request',  field: error.type === 'field'? error.path: 'not found field' });
             })
-            res.status(400).json({ errors: errorMessages });
+            res.status(400).json({ errorsMessages: errorMessages });
         }
 
     next()
